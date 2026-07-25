de40a7455576bbfd053a3111c773b2cfc8e33fd4
danielaarriazu <arriazudaniela@gmail.com>
2026-07-25 08:51:57 -0300

    inicio correcion estados

--- src/routes/presupuesto.routes.ts
+++ src/routes/presupuesto.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import { cotizarPresupuesto } from '../controllers/presupuesto.controller';
 import { authorize } from '../middlewares/authorize.middleware';
+import { verificarToken } from '../middlewares/auth.middleware';
 
 const router = Router();
 
 // Endpoint para que el emprendedor envíe la cotización
-router.put('/:id/cotizar', cotizarPresupuesto, authorize('EMPRENDEDOR'));
+router.put('/:id/cotizar', verificarToken, cotizarPresupuesto, authorize('EMPRENDEDOR'));
 
 export default router;
