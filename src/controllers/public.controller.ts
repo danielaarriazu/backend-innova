52f9814af469ee4cdab65c1117f5db211804e21a
danielaarriazu <arriazudaniela@gmail.com>
2026-07-27 00:29:37 -0300

    v.4

--- src/controllers/public.controller.ts
+++ src/controllers/public.controller.ts
@@ -18,7 +18,7 @@ export const getProductosPublicos = async (
   }
 };
 
-export const getFaqsPublicas = async (
+export const getFAQsPublicas = async (
   req: Request,
   res: Response,
   next: NextFunction
